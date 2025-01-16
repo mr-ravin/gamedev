2eea9862f56960d13c9193607ff2338d01449c4e
Ravin Kumar <16964978+mr-ravin@users.noreply.github.com>
2025-01-16 18:14:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <center><h1><b><i> Game Dev's Life </i></b></h1></center>
+   <br/>
+   <div class="middle">
+     <ol>
+     <li> <b><u>RotationWars</u></b>
+     <p align="justify">
+     A multiplayer indie game developed with the Unity3D game engine for android, where players compete on a rotating platform. They can push each other and maneuver to avoid being pushed off. The goal is to make opponents fall off the platform to claim victory.
+     </p>
+     <p align="justify">
+     What adds to the challenge?
+    <ol>
+    <li>The platform continuously accelerates.</li>
+    <li>Each collision affects the players' control direction, altering how movement buttons respond.</li>
+    </ol>
+     </p>
+     <p align="justify">
+     <u>Play the game</u> :
+     <ul>
+      <li>Google playstore: <a target="_blank" rel="noopener noreferrer" href="https://play.google.com/store/apps/details?id=ravin.developer.rotationwars" target='_blank'>android install</a></li>
+      <li>Play online on itch.io: <a target="_blank" rel="noopener noreferrer" href="https://ravinkumar.itch.io/rotationwars">play online</a></li>
+      <li>Github repository: <a target="_blank" rel="noopener noreferrer" href="https://github.com/mr-ravin/RotationWars">source code</a></li>
+     </ul>
+     </p>
+     </li>
+
+
+     <li> <b><u>QRGame</u></b>
+     <p align="justify">
+     QRGame is a minimalist, terminal-based typing game where the player must quickly type a displayed key and press Enter before time runs out. As players advance, the time to respond shortens, pushing speed and accuracy to the limit. A single mistake ends the game, keeping the tension high as the challenge intensifies. True to its name, QRGame can be stored entirely within a simple QR code, making it easy to share and quick to start!
+     </p>
+     <p align="justify">
+     <u>Play the game</u> :
+     <ul>
+      <li>Github repository: <a target="_blank" rel="noopener noreferrer" href="https://github.com/mr-ravin/QRGame">source code</a></li>
+     </ul>
+     </p>
+     </li>
+
+
+     <li> <b><u>Blocks-N-Head</u></b>
+     <p align="justify">
+     Blocks-N-Head is an open-source game where the objective is to help the character avoid falling blocks.
+     </p>
+     <p align="justify">
+     <u>Play the game</u> :
+     <ul>
+     <li>Play online on itch.io: <a target="_blank" rel="noopener noreferrer" href="https://ravinkumar.itch.io/blocks-n-head">play online</a></li>
+      <li>Github repository: <a target="_blank" rel="noopener noreferrer" href="https://github.com/mr-ravin/Blocks-N-Head">source code</a></li>
+     </ul>
+     </p>
+     </li>
+    </ol>
+    </div>
+    </div>
+  );
+}
+
+export default App;
