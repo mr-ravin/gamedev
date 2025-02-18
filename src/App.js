84d102f10271edccf1847d482283f1b311599dfb
Ravin Kumar <16964978+mr-ravin@users.noreply.github.com>
2025-02-18 15:48:05 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ function App() {
      <ol>
      <li> <b><u>RotationWars</u></b>
      <p align="justify">
-     A multiplayer indie game developed with the Unity3D game engine for android, where players compete on a rotating platform. They can push each other and maneuver to avoid being pushed off. The goal is to make opponents fall off the platform to claim victory.
+     An open-source minimalistic multiplayer video game developed with the Unity3D game engine for android, where players compete on a rotating platform. They can push each other and maneuver to avoid being pushed off. The goal is to make opponents fall off the platform to claim victory.
      </p>
      <p align="justify">
      What adds to the challenge?
     <ol>
     <li>The platform continuously accelerates.</li>
-    <li>Each collision affects the players' control direction, altering how movement buttons respond.</li>
+    <li>Each collision affects the player's control direction, altering how movement buttons respond.</li>
     </ol>
      </p>
      <p align="justify">
@@ -31,7 +31,7 @@ function App() {
 
      <li> <b><u>QRGame</u></b>
      <p align="justify">
-     QRGame is a minimalist, terminal-based typing game where the player must quickly type a displayed key and press Enter before time runs out. As players advance, the time to respond shortens, pushing speed and accuracy to the limit. A single mistake ends the game, keeping the tension high as the challenge intensifies. True to its name, QRGame can be stored entirely within a simple QR code, making it easy to share and quick to start!
+     QRGame is a minimalist, terminal-typing based open-source video game where the player must quickly type a displayed key and press Enter before time runs out. As players advance, the time to respond shortens, pushing speed and accuracy to the limit. A single mistake ends the game, keeping the tension high as the challenge intensifies. True to its name, QRGame can be stored entirely within a simple QR code, making it easy to share and quick to start!
      </p>
      <p align="justify">
      <u>Play the game</u> :
@@ -44,7 +44,7 @@ function App() {
 
      <li> <b><u>Blocks-N-Head</u></b>
      <p align="justify">
-     Blocks-N-Head is an open-source game where the objective is to help the character avoid falling blocks.
+     Blocks-N-Head is an open-source minimalistic video game where the objective is to help the character avoid falling blocks.
      </p>
      <p align="justify">
      <u>Play the game</u> :
